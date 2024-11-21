48d225f4eceb996e2557c6c3dbe79b0cf587503a
suryanshranjan-dev <suryanshranjan.9250@gmail.com>
2024-11-21 20:10:20 +0530

    Added 1 more print statement.

--- Day 1/helloWorld.java
+++ Day 1/helloWorld.java
@@ -2,6 +2,7 @@ public class helloWorld
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hey, Suryansh! Welcome to the course of M.C.A😀");
+		System.out.println("Hey, Suryansh! Welcome to the course of M.C.A😀");4
+		System.out.println("Happy Coding❤️");
 	}
 }
