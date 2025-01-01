2e723aee7b78aa69f002ace396ba98b5fe11e698
suryanshranjan-dev <suryanshranjan.9250@gmail.com>
2025-01-01 22:35:24 +0530

    Learnt operators in java

--- Day 1/helloWorld.java
+++
@@ -1,8 +0,0 @@
-public class helloWorld
-{
-	public static void main(String args[])
-	{
-		System.out.println("Hey, Suryansh! Welcome to the course of M.C.A😀");4
-		System.out.println("Happy Coding❤️");
-	}
-}
